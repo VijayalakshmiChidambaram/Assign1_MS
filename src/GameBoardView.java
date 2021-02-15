d563817554c6687722b00f5de6640d5d13c210ae
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-02-15 11:58:18 -0600

    Update the Grids to grey color

--- src/GameBoardView.java
+++ src/GameBoardView.java
@@ -8,21 +8,23 @@ public class GameBoardView {
         //Create a new Frame(GameBoard)
         JFrame frame = new JFrame("Minesweeper");
         frame.setVisible(true);
-        frame.setSize(800,800);
+        frame.setSize(350,350);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
         //Creating a matrix of 100 cells
-        panel.setLayout(new GridLayout(10,10));
+        panel.setLayout(new GridLayout(10,10,0,0));
         for (int i =0; i<10; i++)
         {
             for(int j=0; j<10; j++)
             {
                 JButton button = new JButton();
-                panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
                 panel.add(button);
+                button.setBackground(Color.lightGray);
             }
         }
+
+        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         frame.add(panel);
         frame.setVisible(true);
 
