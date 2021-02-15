5d412fa8d93fddb996224aa79680b2cdbc68b96b
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-02-15 11:15:16 -0600

    Created Game Board with Grids that hold 100 cells

--- src/GameBoardView.java
+++ src/GameBoardView.java
@@ -1,18 +1,31 @@
-
+import javax.swing.*;
+import java.awt.*;
 
 public class GameBoardView {
-    public static final int height = 800, width = 800;
-    public static final int grid_size = 10;
 
-
-    public GameBoardView() {
-        new GameWindow(height, width, grid_size,"Minesweeper", this);
+    public static void main (String[] args)
+    {
+        //Create a new Frame(GameBoard)
+        JFrame frame = new JFrame("Minesweeper");
+        frame.setVisible(true);
+        frame.setSize(800,800);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        //Creating a matrix of 100 cells
+        panel.setLayout(new GridLayout(10,10));
+        for (int i =0; i<10; i++)
+        {
+            for(int j=0; j<10; j++)
+            {
+                JButton button = new JButton();
+                panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+                panel.add(button);
+            }
+        }
+        frame.add(panel);
+        frame.setVisible(true);
 
     }
-
-    public static void main(String[] args) {
-        new GameBoardView();
-    }
-
 
     }
